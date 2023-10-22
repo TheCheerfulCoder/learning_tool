52ac3153224deb9f639ec8bdef25ccc9d25b7404
JonasFP <jfieldpatton@gmail.com>
2023-10-21 17:28:43 -0700

    Serve an HTML file

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello world!'));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
